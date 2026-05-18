10c4a4028152ea1b5d239a243ff7a552d0e74357
Atrot <celovek2344@gmail.com>
2026-05-19 01:16:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import pandas as pd
+
+def main():
+    # Читаем Excel-файл
+    df = pd.read_excel('sales_data.xlsx')
+
+    # Добавляем колонку с суммой продажи
+    df['Сумма'] = df['Количество'] * df['Цена']
+
+    # Группируем по категориям и считаем общую выручку
+    report = df.groupby('Категория', as_index=False)['Сумма'].sum()
+    report = report.sort_values('Сумма', ascending=False)
+
+    # Сохраняем результат в новый Excel-файл
+    report.to_excel('sales_summary.xlsx', index=False)
+
+    # Выводим результат в консоль
+    print("Отчёт сохранён в sales_summary.xlsx")
+    print(report)
+
+if __name__ == '__main__':
+    main()
+    
